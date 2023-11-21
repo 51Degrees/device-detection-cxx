b08af74614114d48555b85ff57e8df6e7dd5f07b
Automation51D <51DCI@51Degrees.com>
2023-11-21 03:02:00 +0000

    REF: Updated documentation.

--- 4.4/group___fifty_one_degrees_device_detection.js
+++ 4.4/group___fifty_one_degrees_device_detection.js
@@ -1,8 +1,8 @@
 var group___fifty_one_degrees_device_detection =
 [
     [ "Hash API", "group___fifty_one_degrees_hash.html", "group___fifty_one_degrees_hash" ],
-    [ "Config", "group___fifty_one_degrees_config_device_detection.html", "group___fifty_one_degrees_config_device_detection" ],
-    [ "Data Set", "group___fifty_one_degrees_data_set_device_detection.html", "group___fifty_one_degrees_data_set_device_detection" ],
-    [ "Synonyms", "group___fifty_one_degrees_device_detection_synonyms.html", "group___fifty_one_degrees_device_detection_synonyms" ],
-    [ "Results", "group___fifty_one_degrees_device_detection_results.html", "group___fifty_one_degrees_device_detection_results" ]
+    [ "Device Detection Config", "group___fifty_one_degrees_config_device_detection.html", "group___fifty_one_degrees_config_device_detection" ],
+    [ "Device Detection Data Set", "group___fifty_one_degrees_data_set_device_detection.html", "group___fifty_one_degrees_data_set_device_detection" ],
+    [ "Device Detection Synonyms", "group___fifty_one_degrees_device_detection_synonyms.html", "group___fifty_one_degrees_device_detection_synonyms" ],
+    [ "Device Detection Results", "group___fifty_one_degrees_device_detection_results.html", "group___fifty_one_degrees_device_detection_results" ]
 ];
--- 4.4/group___fifty_one_degrees_hash.js
+++ 4.4/group___fifty_one_degrees_hash.js
@@ -1,6 +1,6 @@
 var group___fifty_one_degrees_hash =
 [
-    [ "Synonyms", "group___fifty_one_degrees_hash_synonyms.html", "group___fifty_one_degrees_hash_synonyms" ],
+    [ "Hash Synonyms", "group___fifty_one_degrees_hash_synonyms.html", "group___fifty_one_degrees_hash_synonyms" ],
     [ "Graph", "group___fifty_one_degrees_graph.html", "group___fifty_one_degrees_graph" ],
     [ "Hash", "group___fifty_one_degrees_hash_api.html", "group___fifty_one_degrees_hash_api" ]
 ];
