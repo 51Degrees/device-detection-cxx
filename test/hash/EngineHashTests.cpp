a23d4ee33c4b8d0517a2da624f8b30298bd520d9
Maksym Kucherov <maximus.the0@gmail.com>
2024-01-08 16:26:31 +0200

    Add few more sub-tests into `verifyProcessDeviceIdQuery`.

--- test/hash/EngineHashTests.cpp
+++ test/hash/EngineHashTests.cpp
@@ -24,6 +24,7 @@
 #include "../EngineDeviceDetectionTests.hpp"
 #include "../../src/hash/EngineHash.hpp"
 #include <memory>
+#include <functional>
 
 using namespace FiftyoneDegrees::Common;
 using namespace FiftyoneDegrees::DeviceDetection;
@@ -680,38 +681,78 @@ public:
 
 		// Collect control device IDs
 
-		std::string deviceId_mobile;
-		{
-			// Carries out a match for a mobile User-Agent.
+		std::function<std::string(const char *)> const deviceIdFromUserAgent = [hashEngine](const char* ua) {
 			EvidenceDeviceDetection evidence;
-			evidence["header.user-agent"] = mobileUserAgent;
+			evidence["header.user-agent"] = ua;
 
 			std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-			deviceId_mobile = results->getDeviceId();
+			return results->getDeviceId();
 		};
-		std::string deviceId_mediaHub;
-		{
-			// Carries out a match for a MediaHub User-Agent.
-			EvidenceDeviceDetection evidence;
-			evidence["header.user-agent"] = mediaHubUserAgent;
+		auto const deviceId_mobile = deviceIdFromUserAgent(mobileUserAgent);
+		auto const deviceId_mediaHub = deviceIdFromUserAgent(mediaHubUserAgent);
 
-			std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-			deviceId_mediaHub = results->getDeviceId();
+		std::vector<std::string> const knownIDs = {
+			deviceId_mobile,
+			deviceId_mediaHub,
 		};
 
-		// Inject IDs and check expectations
+		// Build evidence, check expectations
 
 		{
-			// Carries out a match for a platform based on device ID.
-			EvidenceDeviceDetection evidence;
-			evidence["header.user-agent"] = mediaHubUserAgent;
-			evidence["query.51D_deviceId"] = deviceId_mobile.c_str();
+			// Use device ID as the only evidence
+			for (auto it = knownIDs.begin(); it != knownIDs.end(); ++it) {
+				std::string const& nextID = *it;
 
-			std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-			EXPECT_STREQ(deviceId_mobile.c_str(), results->getDeviceId().c_str());
+				EvidenceDeviceDetection evidence;
+				evidence["query.51D_deviceId"] = nextID;
+
+				std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+				EXPECT_STREQ(nextID.c_str(), results->getDeviceId().c_str());
+			};
 		};
+
 		{
-			// Carries out a match for a platform with invalid device ID.
+			// Device ID takes precedence over 'normal' evidence, i.e. user-agent
+			{
+				// MediaHub agent + Mobile deviceID
+				EvidenceDeviceDetection evidence;
+				evidence["header.user-agent"] = mediaHubUserAgent;
+				evidence["query.51D_deviceId"] = deviceId_mobile;
+
+				std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+				EXPECT_STREQ(deviceId_mobile.c_str(), results->getDeviceId().c_str());
+			};
+			{
+				// Mobile agent + MediaHub deviceID
+				// + key case-insensitivity check
+				EvidenceDeviceDetection evidence;
+				evidence["header.user-agent"] = mobileUserAgent;
+				evidence["query.51d_dEVIcEiD"] = deviceId_mediaHub.c_str(); 
+
+				std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+				EXPECT_STREQ(deviceId_mediaHub.c_str(), results->getDeviceId().c_str());
+			};
+		};
+
+		{
+			// Profile ID overrides are applied over detected device ID.
+			const char* const expectedDeviceId = "12280-17779-17470-18092";
+			const char* const evidenceValue = "12280|17779|17470|18092";
+
+			for (auto it = knownIDs.begin(); it != knownIDs.end(); ++it) {
+				std::string const& nextID = *it;
+
+				EvidenceDeviceDetection evidence;
+				evidence["query.ProfileIds"] = evidenceValue;
+				evidence["query.51D_deviceId"] = nextID;
+
+				std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+				EXPECT_STREQ(expectedDeviceId, results->getDeviceId().c_str());
+			};
+		};
+
+		{
+			// Invalid device ID.
 			std::vector<std::string> const deviceId_dummies = {
 				"190706-2244-1242-2412",
 				"190706~2244~1242~2412",
@@ -720,36 +761,24 @@ public:
 				"42",
 			};
 			for (int i = 0; i < deviceId_dummies.size(); ++i) {
-				EvidenceDeviceDetection evidence;
-				evidence["header.user-agent"] = mediaHubUserAgent;
-				evidence["query.51D_deviceId"] = deviceId_dummies[i];
+				{
+					// no match
+					EvidenceDeviceDetection evidence;
+					evidence["query.51D_deviceId"] = deviceId_dummies[i];
 
-				std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-				EXPECT_STREQ(deviceId_mediaHub.c_str(), results->getDeviceId().c_str());
+					std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+					EXPECT_STREQ("0-0-0-0", results->getDeviceId().c_str());
+				};
+				{
+					// fallback
+					EvidenceDeviceDetection evidence;
+					evidence["header.user-agent"] = mediaHubUserAgent;
+					evidence["query.51D_deviceId"] = deviceId_dummies[i];
+
+					std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
+					EXPECT_STREQ(deviceId_mediaHub.c_str(), results->getDeviceId().c_str());
+				};
 			}
-		};
-		{
-			// Carries out a match for a platform with device ID and profile overrides.
-			const char* const expectedDeviceId = "12280-17779-17470-18092";
-			const char* const evidenceValue = "12280|17779|17470|18092";
-
-			EvidenceDeviceDetection evidence;
-			evidence["query.ProfileIds"] = evidenceValue;
-			evidence["query.51D_deviceId"] = deviceId_mediaHub;
-
-			std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-			EXPECT_STREQ(expectedDeviceId, results->getDeviceId().c_str());
-		};
-		{
-			// Carries out a match for a mobile User-Agent with MediaHub DeviceID.
-			EvidenceDeviceDetection evidence;
-
-			evidence["header.user-agent"] = mobileUserAgent;
-			// case-insensitive key
-			evidence["query.51d_dEVIcEiD"] = deviceId_mediaHub.c_str();
-
-			std::unique_ptr<ResultsHash> const results(hashEngine->process(&evidence));
-			EXPECT_STREQ(deviceId_mediaHub.c_str(), results->getDeviceId().c_str());
 		};
 	}
 
