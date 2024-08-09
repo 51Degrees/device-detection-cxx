f14ee702675fcd14d8064a8c944d4216f7501286
Alexander Popel <alexander.popel@postindustria.com>
2024-08-09 17:26:48 +0300

    ci: use common get-next-package-version script

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -1,14 +1,1 @@
-
-param (
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [string]$VariableName
-)
-
-# This is common logic, so let's call the common script
-./steps/get-next-package-version.ps1 -RepoName $RepoName -VariableName "GitVersion"
-
-# Set the variable for the caller
-Set-Variable -Name $VariableName -Value $GitVersion.SemVer -Scope Global
-
-exit $LASTEXITCODE
+. ./cxx/get-next-package-version.ps1 @args
