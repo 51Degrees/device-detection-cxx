6cb6e03caac85e4bce6615fd9bddb1b6c95d9c16
Alexander Popel <alexander.popel@postindustria.com>
2024-08-12 14:25:11 +0300

    ci: fix get-next-package-version
    Allow run-repo-script to see common get-next-package-version params.

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -1,1 +1,8 @@
-. ./cxx/get-next-package-version.ps1 @args
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [string]$VariableName
+)
+
+. ./cxx/get-next-package-version.ps1 -RepoName $RepoName -VariableName $VariableName
