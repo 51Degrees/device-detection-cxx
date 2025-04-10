3a92b189d3d57810f45d95bea1804f59ec107ea0
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-04-10 23:32:32 +0200

    DOC: comment on install-package.ps1

--- ci/install-package.ps1
+++ ci/install-package.ps1
@@ -7,8 +7,10 @@ $WorkDir = [IO.Path]::Combine($pwd, $RepoName)
 $BuildDir = [IO.Path]::Combine($WorkDir, "build")
 
 
-# Copy the prebuilt build dir with binaries and artifacts for tests into workdir
+# Copy the prebuilt build dir with binaries and artifacts for tests into workdir:
 Copy-Item -Path $PackagePath/* -Destination $WorkDir -Recurse
+
+# We probably need just execution permissions (otherwise CTest fails to run HashTests binary), 777 to not bother:
 chmod -R 777 $BuildDir
 
 Write-Output "PackagePath contents:"
